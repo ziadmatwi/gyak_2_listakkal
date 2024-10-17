a3e53b86a10b1588aee5fed103449a27bbc86b1b
ziadmatwi <ziadgabesz@gmail.com>
2024-10-17 11:03:35 +0200

    végleges

--- feladatok.py
+++ feladatok.py
@@ -34,21 +34,118 @@ def ermedobas():
     i:int=0
     a:int=0
     lista = []
-    while i<=10:
-        a=random.choice([0,1])
+    while i<10:
+        a=int(random.random()*2+1)
         lista.append(a)
         i+=1
     return(lista)
+
+def fejszamlalo(lista):
+    i:int=0
+    db:int=0
+    while i < len(lista):
+        if lista[i] == 1:
+            db+=1
+        i+=1
+    return db
 
 def kockadobas():
     i:int=0
     a:int=0
     lista = []
-    while i<=10:
-        a=random.choice([1,2,3,4,5,6])
+    while i<200:
+        a=int(random.random()*6+1)
         lista.append(a)
         i+=1
     return(lista)
 
+def kockaszamlalas(lista):
+    i:int=0
+    egy:int=0
+    ket:int=0
+    harom:int=0
+    negy:int=0
+    ot:int=0
+    hat:int=0
+    while i < len(lista):
+        if lista[i] == 1:
+            egy+=1
+        if lista[i] == 2:
+            ket+=1
+        if lista[i] == 3:
+            harom+=1
+        if lista[i] == 4:
+            negy+=1
+        if lista[i] == 5:
+            ot+=1
+        if lista[i] == 6:
+            hat+=1
+        i+=1
+    return egy, ket, harom, negy, ot, hat
+
+def kockacinkelt():
+    i:int=0
+    a:int=0
+    lista = []
+    while i<200:
+        a=int(random.random()*7+1)
+        lista.append(a)
+        i+=1
+    return(lista)
+
+def kockaszamlalascinkelt(lista):
+    i:int=0
+    egy:int=0
+    ket:int=0
+    harom:int=0
+    negy:int=0
+    ot:int=0
+    hat:int=0
+    while i < len(lista):
+        if lista[i] == 1:
+            egy+=1
+        if lista[i] == 2:
+            ket+=1
+        if lista[i] == 3:
+            harom+=1
+        if lista[i] == 4:
+            negy+=1
+        if lista[i] == 5:
+            ot+=1
+        if lista[i] == 6:
+            hat+=1
+        if lista[i] == 7:
+            hat+=1
+        i+=1
+    return egy, ket, harom, negy, ot, hat
+
+def kockaszamlalasuj(lista,szam):
+    i:int=0
+    db:int=0
+    while i < len(lista):
+        if szam == lista[i]:
+            db+=1
+        i+=1
+    return db
+
+def kockacinkeltuj():
+    i:int=0
+    a:int=0
+    lista = []
+    while i<200:
+        a=int(random.random()*7+1)
+        if a >= 6:
+            lista.append(6)
+        else:
+            lista.append(a)
+        i+=1
+    return(lista)
 
  
+def kockaszamlalasx(lista):
+    i:int=0
+    lista2 = [0,0,0,0,0,0]
+    while i < len(lista):
+        lista2[lista[i] -1 ]+=1
+        i+=1
+    return lista2
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import feladatok
 lista=[12, 21, 56, 32, -5, -23, 35]
 
-print("1.feladat")
+'''print("1.feladat")
 db:int=feladatok.poz_szamok_szama(lista)
 print(f"A pozitív számok száma: {db}")
 
@@ -13,4 +13,46 @@ print("3.feladat")
 db:int=feladatok.ottel_oszthato(lista)
 print(f"Az öttel osztható számok átlaga: {db}")
 
-print(feladatok.ermedobas())
+print("4.feladat")
+print(f"10 érmedobás között ennyi fej található: {feladatok.fejszamlalo(feladatok.ermedobas())}")
+
+print("5.feladat")
+dobaseredmenyek=feladatok.kockaszamlalas(feladatok.kockadobas())
+print("A kockadobás eredményei:")
+print(f"Egyesek: {dobaseredmenyek[0]}")
+print(f"Kettesek: {dobaseredmenyek[1]}")
+print(f"Hármasok: {dobaseredmenyek[2]}")
+print(f"Négyesek: {dobaseredmenyek[3]}")
+print(f"Ötösök: {dobaseredmenyek[4]}")
+print(f"Hatosok: {dobaseredmenyek[5]}")
+
+print("6.feladat")
+dobaseredmenyek=feladatok.kockaszamlalascinkelt(feladatok.kockacinkelt())
+print("A kockadobás eredményei:")
+print(f"Egyesek: {dobaseredmenyek[0]}")
+print(f"Kettesek: {dobaseredmenyek[1]}")
+print(f"Hármasok: {dobaseredmenyek[2]}")
+print(f"Négyesek: {dobaseredmenyek[3]}")
+print(f"Ötösök: {dobaseredmenyek[4]}")
+print(f"Hatosok: {dobaseredmenyek[5]}")'''
+
+print("5.feladat újra:")
+dobások=feladatok.kockadobas()
+print("A kockadobás eredményei:")
+print(f"Egyesek: {feladatok.kockaszamlalasuj(dobások,1)}")
+print(f"Kettesek: {feladatok.kockaszamlalasuj(dobások,2)}")
+print(f"Hármasok: {feladatok.kockaszamlalasuj(dobások,3)}")
+print(f"Négyesek: {feladatok.kockaszamlalasuj(dobások,4)}")
+print(f"Ötösök: {feladatok.kockaszamlalasuj(dobások,5)}")
+print(f"Hatosok: {feladatok.kockaszamlalasuj(dobások,6)}")
+
+
+print("6.feladat újra:")
+dobások=feladatok.kockacinkeltuj()
+print("A kockadobás eredményei:")
+print(f"Egyesek: {feladatok.kockaszamlalasuj(dobások,1)}")
+print(f"Kettesek: {feladatok.kockaszamlalasuj(dobások,2)}")
+print(f"Hármasok: {feladatok.kockaszamlalasuj(dobások,3)}")
+print(f"Négyesek: {feladatok.kockaszamlalasuj(dobások,4)}")
+print(f"Ötösök: {feladatok.kockaszamlalasuj(dobások,5)}")
+print(f"Hatosok: {feladatok.kockaszamlalasuj(dobások,6)}")
