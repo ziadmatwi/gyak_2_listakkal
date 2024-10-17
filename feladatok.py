ba585629862d78ca7e2aa41258d4f6b7108694da
ziadmatwi <ziadgabesz@gmail.com>
2024-10-17 09:17:51 +0200

    git commit -m"2,3" .

--- feladatok.py
+++ feladatok.py
@@ -1,7 +1,54 @@
+import random
+
 def poz_szamok_szama(lista):
     i:int=0
     db:int=0
     while(i<len(lista)):
         if (lista[i]>0):
             db+=1
+        i+=1
     return db
+
+def negativ_szamok_osszege(lista):
+    i:int=0
+    db:int=0
+    while(i<len(lista)):
+        if (lista[i]<0):
+            db+=1
+        i+=1
+    return db
+
+def ottel_oszthato(lista):
+    i:int=0
+    db:int=0
+    a:int=0
+    while (i<len(lista)):
+        if lista[i] % 5 == 0:
+            a+=lista[i]
+            db+=1
+        i+=1
+    return a/db
+
+
+def ermedobas():
+    i:int=0
+    a:int=0
+    lista = []
+    while i<=10:
+        a=random.choice([0,1])
+        lista.append(a)
+        i+=1
+    return(lista)
+
+def kockadobas():
+    i:int=0
+    a:int=0
+    lista = []
+    while i<=10:
+        a=random.choice([1,2,3,4,5,6])
+        lista.append(a)
+        i+=1
+    return(lista)
+
+
+ 
--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import feladatok
 lista=[12, 21, 56, 32, -5, -23, 35]
 
 print("1.feladat")
-db:int=feladatok.poz_szamok_szama
+db:int=feladatok.poz_szamok_szama(lista)
 print(f"A pozitív számok száma: {db}")
+
+print("2.feladat")
+db:int=feladatok.negativ_szamok_osszege(lista)
+print(f"A negatív számok száma: {db}")
+
+print("3.feladat")
+db:int=feladatok.ottel_oszthato(lista)
+print(f"Az öttel osztható számok átlaga: {db}")
+
+print(feladatok.ermedobas())
